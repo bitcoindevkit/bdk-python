3facd1bc634c7e6b5d885c8fd3dd3e31e940eec7
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-05-19 15:14:43 -0400

    test: update tests for 3.0 API

--- tests/test_offline_descriptor.py
+++ tests/test_offline_descriptor.py
@@ -2,7 +2,7 @@ from bdkpython import Descriptor
 from bdkpython import Mnemonic
 from bdkpython import DescriptorSecretKey
 from bdkpython import KeychainKind
-from bdkpython import Network
+from bdkpython import NetworkKind
 
 import unittest
 
@@ -10,8 +10,8 @@ class OfflineDescriptorTest(unittest.TestCase):
 
     def test_descriptor_bip86(self):
         mnemonic: Mnemonic = Mnemonic.from_string("space echo position wrist orient erupt relief museum myself grain wisdom tumble")
-        descriptor_secret_key: DescriptorSecretKey = DescriptorSecretKey(Network.TESTNET, mnemonic, None)
-        descriptor: Descriptor = Descriptor.new_bip86(descriptor_secret_key, KeychainKind.EXTERNAL, Network.TESTNET)
+        descriptor_secret_key: DescriptorSecretKey = DescriptorSecretKey(NetworkKind.TEST, mnemonic, None)
+        descriptor: Descriptor = Descriptor.new_bip86(descriptor_secret_key, KeychainKind.EXTERNAL, NetworkKind.TEST)
 
         self.assertEqual(
             "tr([be1eec8f/86'/1'/0']tpubDCTtszwSxPx3tATqDrsSyqScPNnUChwQAVAkanuDUCJQESGBbkt68nXXKRDifYSDbeMa2Xg2euKbXaU3YphvGWftDE7ozRKPriT6vAo3xsc/0/*)#m7puekcx",
--- tests/test_offline_wallet.py
+++ tests/test_offline_wallet.py
@@ -4,17 +4,18 @@ from bdkpython import KeychainKind
 from bdkpython import Persister
 from bdkpython import AddressInfo
 from bdkpython import Network
+from bdkpython import NetworkKind
 
 import unittest
 import os
 
 descriptor: Descriptor = Descriptor(
     "wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/1h/0/*)",
-    Network.TESTNET
+    NetworkKind.TEST
 )
 change_descriptor: Descriptor = Descriptor(
     "wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/1h/1/*)",
-    Network.TESTNET
+    NetworkKind.TEST
 )
 
 class OfflineWalletTest(unittest.TestCase):
